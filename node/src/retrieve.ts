cb0eb42ca3dc4860c749ac79792ed95c6094de6d
Abhay Upadhyay <abhay281103@gmail.com>
2026-03-31 04:58:55 +0530

    fix: sql query

--- node/src/retrieve.ts
+++ node/src/retrieve.ts
@@ -17,7 +17,7 @@ import pg from "pg";
 const { Client } = pg;
 
 const RESOLVE_SQL = `
-  SELECT pr.piece_ref, pp.piece_raw_size
+  SELECT pp.id AS piece_ref, pp.piece_raw_size
   FROM curio.pdp_data_set_pieces dsp
   JOIN curio.pdp_piecerefs pr ON pr.id = dsp.pdp_pieceref
   JOIN curio.parked_piece_refs pprf ON pprf.ref_id = pr.piece_ref
